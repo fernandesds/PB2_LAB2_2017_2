3eabac7b09c4523e4e5e7aff5bd290b320d6769b
Guilherme Fernandes dos Santos <guih_cpa@hotmail.com>
2017-12-06 20:14:56 -0300

    Adcionando Class

---
+++ WebAppProva2bLab2Guilherme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProva2bLab2Guilherme.Startup))]
+namespace WebAppProva2bLab2Guilherme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
